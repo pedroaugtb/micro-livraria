e239e6f6adc4e514dabb66a6593335c181bde21e
Pedro Augusto Torres Bento <127890688+pedroaugtb@users.noreply.github.com>
2025-05-25 23:47:24 -0300

    Update index.js

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -1,28 +1,44 @@
-const grpc = require('@grpc/grpc-js');
+const path        = require('path');
+const grpc        = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
-const products = require('./products.json');
+const products    = require('./products.json');
 
-const packageDefinition = protoLoader.loadSync('proto/inventory.proto', {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    arrays: true,
-});
-
-const inventoryProto = grpc.loadPackageDefinition(packageDefinition);
+// carrega a definição .proto
+const packageDef = protoLoader.loadSync(
+  path.join(__dirname, '../../proto/inventory.proto'),
+  { keepCase: true, longs: String, enums: String, defaults: true, oneofs: true }
+);
+const inventoryProto = grpc.loadPackageDefinition(packageDef).inventory;
 
 const server = new grpc.Server();
 
-// implementa os métodos do InventoryService
 server.addService(inventoryProto.InventoryService.service, {
-    searchAllProducts: (_, callback) => {
-        callback(null, {
-            products: products,
-        });
-    },
+  // operação já existente
+  SearchAllProducts: (_, callback) => {
+    callback(null, { products });
+  },
+
+  // ---------- NOVA OPERAÇÃO ----------
+  SearchProductByID: (payload, callback) => {
+    const id      = Number(payload.request.id);
+    const product = products.find(p => p.id === id);
+
+    if (!product) {
+      return callback({
+        code: grpc.status.NOT_FOUND,
+        details: 'Product not found',
+      });
+    }
+    callback(null, product);
+  }
+  // -----------------------------------
 });
 
-server.bindAsync('127.0.0.1:3002', grpc.ServerCredentials.createInsecure(), () => {
-    console.log('Inventory Service running at http://127.0.0.1:3002');
+server.bindAsync(
+  '0.0.0.0:3002',
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    console.log('Inventory Service running');
     server.start();
-});
+  }
+);
